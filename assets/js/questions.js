f16744e7568426c2d9a9753cf030033044a75f23
karollouize <karollouize@gmail.com>
2023-05-03 17:04:27 -0300

    ajustes quiz-rodando finalmente

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -21,7 +21,7 @@ export default [
       question: "Qual foi a contribuição de Hedy Lamarr para a tecnologia moderna?",
       answers: [
         { option: "Invenção do telefone sem fio", correct: false },
-        { option: "Desenvolvimento da televisão de plasma", correct: false}
+        { option: "Desenvolvimento da televisão de plasma", correct: false},
         { option: "Criação do Wi-Fi, GPS e Bluetooth", correct: true },
         { option: "Descoberta da eletricidade", correct: false },
       ],
@@ -30,7 +30,7 @@ export default [
       question: "O que é o sistema de comunicação secreto criado por Hedy Lamarr durante a Segunda Guerra Mundial?",
       answers: [
         { option: "Um dispositivo de controle de tráfego de rua", correct: false },
-        { option: "Um mecanismo para mudar o ângulo das asas de aeronaves", correct: false}
+        { option: "Um mecanismo para mudar o ângulo das asas de aeronaves", correct: false},
         { option: "Uma técnica de comunicação chamada 'salto de frequência'", correct: true },
         { option: "Um sistema de transmissão de imagens em alta definição", correct: false },
       ],
@@ -39,7 +39,7 @@ export default [
         question: "Que prêmio Hedy Lamarr recebeu em 1997 por suas contribuições para a tecnologia moderna?",
         answers: [
           { option: "Prêmio Nobel de Física", correct: false },
-          { option: "Prêmio Oscar", correct: false}
+          { option: "Prêmio Oscar", correct: false},
           { option: "Prêmio Grammy", correct: false },
           { option: "Prêmio BULBIE Gnass Spirit of Achievement", correct: true },
         ],
